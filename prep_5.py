0b61aed5b8bb4a19e1ce29ed6c1acf1021acf8bf
Dovud-Asadi <162010427+Dovud-Asadi@users.noreply.github.com>
2024-08-22 14:17:13 +0500

    Update prep_5.py

--- prep_5.py
+++ prep_5.py
@@ -7,7 +7,7 @@ client = Groq(
     api_key="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", #0731
 )
 
-directory = 'data\folder_5'
+directory = 'unzipped_folder\data_new_filtered\folder_5'
 
 system_message_modified = {
     "role": "system",
